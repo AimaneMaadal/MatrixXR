0a19ff3417ef9fc60ee6f3586ad1aca006bc790b
aimane maadal <aimanemaadal@gmail.com>
2023-04-03 10:32:46 +0200

    fix

--- src/componenets/Loading.jsx
+++ src/componenets/Loading.jsx
@@ -1,28 +1,15 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 import { useFrame } from '@react-three/fiber';
-import { useState, useEffect } from 'react';
-
 const App = () => {
-  const [loadingText, setLoadingText] = useState('loading');
-  const [dots, setDots] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDots(dots => (dots + 1) % 4);
-    }, 400);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    setLoadingText('loading' + '.'.repeat(dots));
-  }, [dots]);
 
   return (
-    <div className='container'>
-      <p className='loading'>{loadingText}</p>
+    <div className='Loading-container'>
+      <img src="load.gif" alt="load" />
+      loading...
     </div>
   )
 }
+
+
 
 export default App
